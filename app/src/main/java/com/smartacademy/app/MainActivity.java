bf73864ad1690c4f661f7bc07a3e8b10d8333f9c
patrickprakash <patrickprakash8@gmail.com>
2020-09-04 16:00:28 +0530

    Initial commit

---
+++ app/src/main/java/com/smartacademy/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.smartacademy.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "http://smartacademy.info/";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+}
